8b7ba9a55e2627b69715fefe192d2912c058a4c0
Stefan Wobbe <wobbe.sf@kubuntu_spin-5>
2018-03-13 19:23:59 +0100

    klausur2017 aufgabe3 added

--- hpk_ws1718/src/klausur2017_java/Aufgabe3.java
+++ hpk_ws1718/src/klausur2017_java/Aufgabe3.java
@@ -1,5 +1,26 @@
 package klausur2017_java;
 
-public class Aufgabe3 {
+import java.lang.reflect.Array;
+import java.util.*;
 
+interface IPrototype<T extends IPrototype> { };
+
+public class Aufgabe3<T extends IPrototype<T>> {
+	
+	public List<T> create(T prototype, int n) {
+		List<T> list;
+		T[] array = (T[]) Array.newInstance(prototype.getClass(), n);
+		list = new ArrayList<T>(Arrays.asList(array));
+		return list;
+	}
+	
+	public T create(T prototype) throws Exception {
+		Class<T> clazz = (Class<T>) prototype.getClass();
+		return clazz.newInstance();
+	}
+	
+	public T create(Class<T> type) throws Exception {
+		return type.newInstance();
+	}
+	
 }
